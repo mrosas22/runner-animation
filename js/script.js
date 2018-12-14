5162df88e6712e557970396f98dc49ac81e6465a
mrosas22 <miller.rosas22@gmail.com>
2018-12-13 21:26:12 -0500

    Different approach but still failing

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ var requestAnimFrame = (function(){
 })();
 
 // Create the canvas
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 1050;
 canvas.height = 350; 
 document.body.appendChild(canvas);
@@ -36,90 +36,64 @@ resources.load([
 resources.onReady(init);
 
 //global variables
-var terrainPattern;
+let terrainPattern;
 
-//A vector for a 2d space
-function Vector(x, y) {
-    // position
-    this.x = x || 0;
-    this.y = y || 0;
-  }
 
-function SpriteSheet(path, frameWidth, frameHeight) {
+
+  function SpriteSheet(path, spriteWidth, spriteHeight) {
+    //the with and height of our spritesheet
+    this.spriteWidth = spriteWidth; 
+    this.spriteHeight = spriteHeight; 
+
     this.image = new Image();
-    this.frameWidth = frameWidth;
-    this.frameHeight = frameHeight;
     // calculate the number of frames in a row after the image loads
-    var self = this;
+    // var self = this;
     this.image.onload = function() {
-      self.framesPerRow = Math.floor(self.image.width / self.frameWidth);
+    //   self.framesPerRow = (self.image.width / self.frameWidth);
     };
    
     this.image.src = path;
-
-}
-
-function Animation(spritesheet, frameSpeed, startFrame, endFrame) {
- 
-    var animationSequence = [];  // array holding the order of the animation
-    var currentFrame = 0;        // the current frame to draw
-    var counter = 0;             // keep track of frame rate
-   
-    // create the sequence of frame numbers for the animation
-    for (var frameNumber = startFrame; frameNumber <= endFrame; frameNumber++)
-      animationSequence.push(frameNumber);
-   
-    // Update the animation
-    this.update = function() {
-   
-      // update to the next frame if it is time
-      if (counter == (frameSpeed - 1))
-        currentFrame = (currentFrame + 1) % animationSequence.length;
-   
-      // update the counter
-      counter = (counter + 1) % frameSpeed;
-    };
-   
-    // draw the current frame
-    this.draw = function(x, y) {
-      // get the row and col of the frame
-      var row = Math.floor(animationSequence[currentFrame] / spritesheet.framesPerRow);
-      var col = Math.floor(animationSequence[currentFrame] % spritesheet.framesPerRow);
-   
-      ctx.drawImage(
-        spritesheet.image,
-        col * spritesheet.frameWidth, row * spritesheet.frameHeight,
-        spritesheet.frameWidth, spritesheet.frameHeight,
-        x, y,
-        spritesheet.frameWidth, spritesheet.frameHeight);
-    };
   }
 
-//CREATE PLAYER
-function Player(x,y){
-    this.width = 144;
-    this.height = 194;
-    // this.x = x;
-    // this.y = y;
-    this.sheet = new SpriteSheet('./images/running.png', this.width, this.height);
-    this.walk = new Animation (this.sheet, 20, 0,5);
-    this.anim = this.walk;
-    Vector.call(this, x, y);
-    this.draw = function(){
-        this.anim.draw(this.x, this.y);
+  function Animation(spritesheet, speed, curFrame, frameCount) {
+
+    //we are having two rows and 8 cols in the current sprite sheet
+    let rows = 1; 
+    let cols = 6; 
+    let width = spritesheet.spriteWidth/cols; 
+    //Same for the height we divided the height with number of rows 
+    let height = spritesheet.spriteHeight/rows;
+   
+    //x and y coordinates to render the sprite 
+    let x=0;
+    let y=0; 
+    //x and y coordinates of the canvas to get the single frame 
+    let srcX=0; 
+    let srcY=0; 
+    //Assuming that at start the character will move right side 
+    // let left = false; 
+    let right = true;
+    this.update = function() {
+        curFrame = ++curFrame % frameCount; 
+        srcX = curFrame * width; 
+        ctx.clearRect(x,y,width,height);	
+        if(right && x < canvas.width - width){
+            // srcY = trackRight * height; 
+            x += speed; 
+        }
     };
-    this.run = function(){
-        this.anim.update(this.x, this.y);
+    this.draw = function(){
+        ctx.drawImage(spritesheet.image,srcX,srcY, width,height,x,y,width,height)
     }
-}
-Player.prototype = Object.create(Vector.prototype);
+   
+  }
 
 
-// spritesheet = new SpriteSheet('./images/running.png', 144, 194);
-// walk = new Animation(spritesheet, 20, 0, 1);
+spritesheet = new SpriteSheet('./images/running.png', 997, 206);
+walk = new Animation(spritesheet, 10, 0, 5);
 
 // Game state
-let player = new Player(15, 15)
+// let player = new Player(15, 15)
 
 
 
@@ -128,36 +102,14 @@ function update(){
     ctx.fillStyle = terrainPattern;
     ctx.clearRect(0,0,canvas.width,canvas.height);
     //Updating the frame 
-    player.run();
+    // player.run();
+    // walk.update();
     //Drawing the image 
-    player.draw();
+    // player.draw();
+    walk.draw();
     // ctx.drawImage(character,srcX,srcY,width,height,x,y,width,height);
 }
- //handle input
- function moveForward(){
-    player.run();
- }
 
-//move player
-document.onkeydown = function(event){
-    // console.log(event.keyCode);
-    switch(event.keyCode){
-        // case 37:// back
-        //     moveBackwards();
-        //     break;
-        case 39: // front
-        console.log('please move')
-            moveForward();
-            break;
-        // case 38: // jump
-        //     player.speedY -= 10;
-        //     break; 
-        // case 16: // shoot
-        //     player.shoot()
-        //     break;
-    }
-}
-// function moveRight(){
-//     right = true; 
-//    }
-//  setInterval(draw, 120);
+function moveRight(){
+    right = true; 
+   }
